32b82c5cc84d12da1ed4be71376c93e39144e3a5
John Crash <udsn2@sina.com>
2015-06-16 20:35:47 +0800

    开始加入启动框架

--- build/src/sdlmain.c
+++ build/src/sdlmain.c
@@ -1,8 +1,60 @@
 #include "sdlmain.h"
+#include <stdio.h>
+
+static SDLState _state;
+
+SDLState *defaultSDLState(int argc,char **argv)
+{
+	memset(&_state, 0, sizeof(_state));
+
+	_state.argc = argc;
+	_state.argv = argv;
+
+	_state.flags = SDL_INIT_VIDEO;
+
+	_state.window_flags = 0;
+	_state.window_x = 0;
+	_state.window_y = 0;
+	_state.window_w = DEFAULT_WINDOW_WIDTH;
+	_state.window_h = DEFAULT_WINDOW_HEIGHT;
+
+	_state.audiospec.freq = 22050;
+	_state.audiospec.format = AUDIO_S16;
+	_state.audiospec.channels = 2;
+	_state.audiospec.samples = 2048;
+	return &_state;
+}
 
 int initSDL(SDLState *state)
 {
-
+	int i,n;
+	if (state->flags & SDL_INIT_VIDEO) {
+		if (state->verbose & VERBOSE_VIDEO) {
+			n = SDL_GetNumVideoDrivers();
+			if (n == 0) {
+				fprintf(stderr, "No built-in video drivers\n");
+			}
+			else {
+				fprintf(stderr, "Built-in video drivers:");
+				for (i = 0; i < n; ++i) {
+					if (i > 0) {
+						fprintf(stderr, ",");
+					}
+					fprintf(stderr, " %s", SDL_GetVideoDriver(i));
+				}
+				fprintf(stderr, "\n");
+			}
+		}
+		if (SDL_VideoInit(state->videodriver) < 0) {
+			fprintf(stderr, "Couldn't initialize video driver: %s\n",
+				SDL_GetError());
+			return SDL_FALSE;
+		}
+		if (state->verbose & VERBOSE_VIDEO) {
+			fprintf(stderr, "Video driver: %s\n",
+				SDL_GetCurrentVideoDriver());
+		}
+	}
 }
 
 void releaseSDL(SDLState *state)
--- build/src/sdlmain.h
+++ build/src/sdlmain.h
@@ -2,22 +2,48 @@
 #define _SDLMAIN_H_
 #include "SDL.h"
 
+#define DEFAULT_WINDOW_WIDTH  640
+#define DEFAULT_WINDOW_HEIGHT 480
+
+#define VERBOSE_VIDEO   0x00000001
+#define VERBOSE_MODES   0x00000002
+#define VERBOSE_RENDER  0x00000004
+#define VERBOSE_EVENT   0x00000008
+#define VERBOSE_AUDIO   0x00000010
+
 #ifdef __cplusplus
 extern "C"{
 #endif
 	typedef struct{
+		/* SDL init flags */
+		int argc;
+		char **argv;
+		Uint32 flags;
+		Uint32 verbose;
+
+		/* Video info */
+		const char *videodriver;
 		const char *window_title;
 		const char *window_icon;
 		Uint32 window_flags;
 		int window_x;
-		int window_h;
+		int window_y;
 		int window_w;
 		int window_h;
 		int window_minW;
 		int window_minH;
 		int window_maxW;
 		int window_maxH;
+
+		/* Audio info */
+		const char *audiodriver;
+		SDL_AudioSpec audiospec;
+
 	} SDLState;
+
+	SDLState *defaultSDLState(int argc, char **argv);
+	int initSDL(SDLState *state);
+	void releaseSDL(SDLState *state);
 #ifdef __cplusplus
 }
 #endif
