861dc8110bf75011d21f266f40d2e8ad9b0d2913
john <udsn2@sina.com>
2016-11-01 12:29:48 +0800

    bitree

--- src/nanovg/bitree.c
+++ src/nanovg/bitree.c
@@ -6,31 +6,62 @@
 * \addtogroup bitree bitree bitree
 * \brief 二分法对空间进行分割查找
 *
-* 
+* 将任何一个数v表示为如下结构,v = 2^m*(2n+1)，其中m=0,1,2,3...,n=0,1,2,3
+* 那么如果n = 0,2,4...那么v是左枝，如果n = 1,3,5...那么v是右枝。
+* 值v的左子枝是v-2^(m-1)，v的右子枝是v+2^(m-1)。v的父节点值是v+-2^m如果v是左值就+，右值就-
 * @{
 */
-static int pow2table[] = {
+int pow2table[] = {
 	1, 1 << 1, 1 << 2, 1 << 3, 1 << 4, 1 << 5, 1 << 6, 1 << 7, 1 << 8,
-	1<<9, 1 << 10, 1 << 11, 1 << 12, 1 << 13, 1 << 14, 1 << 15, 1 << 16, 
-	1<<17, 1 << 18, 1 << 19, 1 << 20, 1 << 21, 1 << 22, 1 << 23, 1 << 24,
-	1<<25, 1 << 26, 1 << 27, 1 << 28, 1 << 29, 1 << 30, 1 << 31,
+	1 << 9, 1 << 10, 1 << 11, 1 << 12, 1 << 13, 1 << 14, 1 << 15, 1 << 16,
+	1 << 17, 1 << 18, 1 << 19, 1 << 20, 1 << 21, 1 << 22, 1 << 23, 1 << 24,
+	1 << 25, 1 << 26, 1 << 27, 1 << 28, 1 << 29, 1 << 30, -(1 << 30)
 };
 
-int bi_level(int v)
+/**
+ * \breif 对一个值做二分分解，v = 2^m(2n+1) ,m=0,1,2..,n=0,1,2..
+ * \param m 返回m值
+ * \param n 返回n值
+ */
+void bi_decompose(int v, int *m, int *n)
 {
 	int i = 0;
-	if (v & 1 || v == 0)return i;
-	i++;
-	while (!((v >>= 1) & 1))
+	if (v == 0){
+		*m = 31;
+		*n = 0;
+		return;
+	}
+	while (!(v & 1)){
+		v >>= 1;
 		i++;
-	return i;
+	}
+	*m = i;
+	*n = (v - 1) >> 1;
 }
 
-#define PARENT_VALUE(v) v
-#define LOW_VALUE(v) v
-#define LEFT_VALUE(v) (v - pow2table[bi_level(v) - 1])
-#define RIGHT_VALUE(v) (v + pow2table[bi_level(v) - 1])
+/**
+ * \breif 二分法，返回值对应的节点的范围
+ * \param minx 返回最小值
+ * \param maxx 返回最大值
+ */
+void bi_range(int v, int *minx, int *maxx)
+{
+	int m, n, pm, pn;
+	if (v == 0){
+		*minx = -(1 << 31 - 1);
+		*maxx = (1 << 31 - 1);
+	}
+	else{
+		bi_decompose(v, &m, &n);
+		*minx = pow2table[m + 1] * n + 1;
+		*maxx = pow2table[m + 1] * (n + 1) - 1;
+	}
+}
 
+/**
+ * \brief 创建一个树节点，该节点的值为v
+ * \return 成功返回树节点，失败返回NULL
+ */
 bitree * bitree_create(int v)
 {
 	bitree* pi = (bitree*)malloc(sizeof(bitree));
@@ -41,54 +72,55 @@ bitree * bitree_create(int v)
 	return pi;
 }
 
-static void bitree_left_delete(bitree * pbt)
-{
-}
-
 /**
  * \brief 删除树相连接的全部节点
  * \param pbt 搜索节点起点
  */
 void bitree_delete(bitree * pbt)
 {
-	bitree * it = pbt;
-	bitree * l = it;
-	while (it){
-		l = it;
-		it = it->l;
-	}
-	bitree_left_delete(l);
 }
 
+/**
+ * \brief 向树上加入一个节点，并将该节点连接到枝干上
+ * \param pbt 主枝干上的任意一个节点
+ * \param v 要加入的节点值，如果已经存在就直接返回该节点
+ */
 bitree * bitree_add(bitree * pbt, int v)
 {
+	int m, n;
 	bitree * it = pbt;
-	bitree * last = it;
 	while (it){
-		if (v > (it->value >> 1)){
-			if (it->p)
-				it = it->p;
-			else{
-				int pv = PARENT_VALUE(it->value);
-				it->p = bitree_create(pv);
-				if (it->value == LEFT_VALUE(pv))
-					it->p->l = it;
-				else
-					it->p->r = it;
-				it = it->p;
-			}
+		if (bitree_ischild(it, v))
+			break;
+		else if (!it->p){
+			bi_decompose(it->value, &m, &n);
+			it->p = bitree_create(BI_PARENT_VALUE(it->value, m,n));
+			if (n % 2)
+				it->p->r = it;
+			else
+				it->p->l = it;
 		}
-		else break;
+		it = it->p;
 	}
+	/*
+	 * 向下查找没有就添加节点
+	 */
 	while (it){
 		if (it->value > v){
-			if (!it->l)
-				it->l = bitree_create(LEFT_VALUE(it->value));
+			if (!it->l){	
+				bi_decompose(it->value, &m, &n);
+				it->l = bitree_create(BI_LEFT_VALUE(it->value,m));
+				it->l->p = it;
+			}
 			it = it->l;
 		}
 		else if (it->value < v){
-			if (!it->r)
-				it->r = bitree_create(RIGHT_VALUE(it->value));
+			if (!it->r){
+				int m, n;
+				bi_decompose(it->value, &m, &n);
+				it->r = bitree_create(BI_RIGHT_VALUE(it->value,m));
+				it->r->p = it;
+			}
 			it = it->r;
 		}
 		else
@@ -104,7 +136,9 @@ bitree * bitree_add(bitree * pbt, int v)
  */
 int bitree_ischild(bitree * pbt, int v)
 {
-	return (v < PARENT_VALUE(pbt->value)) && (v > LOW_VALUE(pbt->value));
+	int minx, maxx;
+	bi_range(pbt->value, &minx, &maxx);
+	return v >= minx && v <= maxx;
 }
 
 /**
@@ -119,7 +153,7 @@ bitree * bitree_at(bitree * pbt, int v)
 	/* 
 	 * 先向上搜索，找到第一个包括v的节点
 	 */
-	while (it && bitree_ischild(it,v)){
+	while (it && !bitree_ischild(it,v)){
 		it = it->p;
 	}
 	/*
@@ -161,7 +195,7 @@ static bitree * bitree_near(bitree * pbt, int v)
 	/*
 	 * 先向上搜索，找到第一个包括v的节点
 	 */
-	while (it && bitree_ischild(it, v)){
+	while (it && !bitree_ischild(it, v)){
 		it = it->p;
 	}
 	/*
--- src/nanovg/bitree.h
+++ src/nanovg/bitree.h
@@ -17,4 +17,16 @@ bitree * bitree_root(bitree * pbt);
 int bitree_ischild(bitree * pbt,int v);
 void bitree_range(bitree * pbt, int v0, int v1, void(*cb)(bitree *it));
 
+int pow2table[];
+void bi_decompose(int v, int *m, int *n);
+void bi_range(int v, int *minx, int *maxx);
+
+/*
+ * 将大于0和小于0的连接到0节点上,这样将整个int全部整合在一个棵树上
+ */
+#define BI_PARENT_VALUE(v,m,n) (m >= 30 ? 0:(n % 2 ? v - pow2table[m] : v + pow2table[m]))
+#define BI_LEFT_VALUE(v,m) (m == 31 ? pow2table[31] : v - pow2table[m - 1])
+#define BI_RIGHT_VALUE(v,m) (m == 31 ? -pow2table[31] : v + pow2table[m - 1])
+#define BI_VALUE(m,n) (m == 31 ?0:pow2table[m]*(2 * n + 1))
+
 #endif
